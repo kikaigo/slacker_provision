1bec35744d759967001e72145b95ff38778942f4
mike hoover <mike.hoover@gmail.com>
2015-05-19 15:49:22 +0000

    docker role and some arcane ssh config

--- ping.sh
+++ ping.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 REMOTE_USER='root' # usually ubuntu
+ANSIBLE_REMOTE_TEMP=/tmp/.ansible/tmp
 export ANSIBLE_HOSTS=./hosts
-ansible-playbook -s -u ${REMOTE_USER} docker.yml
+ansible slack -u ${REMOTE_USER} -m ping
